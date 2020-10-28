5cad5bf64787473304d7659fc6e483dfb0f5db64
Mikel Bishop <mdrew8@gmail.com>
2020-10-28 02:48:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,4 +31,13 @@ class Student
     DB[:conn].execute(sql)
   end 
   
+  def save
+    sql = <<-SQL
+    INSERT INTO students(name, grade)
+    VALUES(?, ?)
+    SQL 
+    DB
+    [:conn].execute(sql, selc.name, self.grade)
+  end 
+  
 end
